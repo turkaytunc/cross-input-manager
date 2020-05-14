ca0887702ea8ed2403314dfa57ef3172e336856f
Turkay Tunc <turkaytunc00@gmail.com>
2020-05-14 15:43:06 +0300

    update scene

---
+++ Assets/FPS Controller/Scripts/MouseLook.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [Header("Mouse Sensivity")]
+    [SerializeField] [Range(100f, 200f)] private float mouseSensivityXAxis = 150f;
+    [SerializeField] [Range(100f, 200f)] private float mouseSensivityYAxis = 130f;
+
+
+    [SerializeField] private Transform playerControllerTransform;
+    [SerializeField] private Transform playerCameraTransform;
+
+    [Header("Camera X Rotation")]
+    [SerializeField] [Range(-60f, -40f)] private float minCameraXRotationAngle = -50f;
+    [SerializeField] [Range(40f, 60f)] private float maxCameraXRotationAngle = 50f;
+    
+
+    private Vector2 inputValue;
+    private Vector3 tempAngles = Vector3.zero;
+
+    void Update()
+    {
+        GetPlayerInput();
+
+        LookAtX();
+        LookAtY();
+    }
+
+    private void LookAtX()
+    {
+        playerControllerTransform.Rotate(new Vector3(0, inputValue.x, 0));
+    }
+
+    private void LookAtY()
+    {
+        tempAngles.x -= inputValue.y;
+        tempAngles.x = Mathf.Clamp(tempAngles.x, minCameraXRotationAngle, maxCameraXRotationAngle);
+        playerCameraTransform.localRotation = Quaternion.Euler(tempAngles);
+    }
+
+    private void GetPlayerInput()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        float mouseXInput = Input.GetAxis("Mouse X") * mouseSensivityXAxis * Time.deltaTime;
+        float mouseYInput = Input.GetAxis("Mouse Y") * mouseSensivityYAxis * Time.deltaTime;
+        
+        inputValue = new Vector2(mouseXInput, mouseYInput);   
+    }
+}
---
+++ Assets/FPS Controller/Scripts/PlayerMovement.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController characterController;
+    [SerializeField] private float movementSpeed;
+
+    [Header("Gravity Options")]
+    [SerializeField] [Range(-80f, 0f)] private float gravityScale = -40f;
+    [SerializeField] [Range(2f, 30f)] private float jumpHeight = 5f;
+
+    [Header("Ground Options")]
+    [SerializeField] private Transform groundCheckTransform;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask whatIsGround;
+
+    [Header("Button Options")]
+    [SerializeField] private string jumpButton = "Jump";
+
+    private Vector3 movementDirection;
+    private Vector3 verticalVelocity;
+    private bool canDoubleJump;
+    private bool isGrounded;
+
+    public bool CanDoubleJump { get => canDoubleJump; set => canDoubleJump = value; }
+    public bool IsGrounded { get => isGrounded; set => isGrounded = value; }
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();        
+    }
+
+    void Update()
+    {
+        GroundCheck();
+        HorizontalMovement();
+        VerticalMovement();
+        Jump();
+
+    }
+
+    private void Jump()
+    {
+        if (IsGrounded && Input.GetButtonDown(jumpButton))
+        {
+            verticalVelocity.y = Mathf.Sqrt(-2 * jumpHeight * gravityScale);
+            CanDoubleJump = true;
+        }
+        else if (Input.GetButtonDown(jumpButton) && CanDoubleJump)
+        {
+            CanDoubleJump = false;
+            verticalVelocity.y = Mathf.Sqrt(-2 * jumpHeight * gravityScale);
+        }
+    }
+
+    private void VerticalMovement()
+    {
+        if (IsGrounded && verticalVelocity.y < 0f)
+        {
+            verticalVelocity.y = 0f;
+        }
+        else
+        {
+            verticalVelocity.y += gravityScale * Time.deltaTime;
+        }
+        characterController.Move(verticalVelocity * Time.deltaTime);
+    }
+
+    private void HorizontalMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        movementDirection = transform.TransformDirection(movementDirection);
+
+        characterController.Move(movementDirection * movementSpeed * Time.deltaTime);
+    }
+
+    private void GroundCheck()
+    {
+        IsGrounded = Physics.CheckSphere(groundCheckTransform.position, groundCheckRadius, whatIsGround);
+    }
+
+}
