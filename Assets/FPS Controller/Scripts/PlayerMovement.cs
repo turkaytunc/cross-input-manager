d822ac32b0bebdbb324cc055da87c0d4414c0958
Turkay Tunc <turkaytunc00@gmail.com>
2020-05-16 15:03:52 +0300

    update player movement

--- Assets/FPS Controller/Scripts/PlayerMovement.cs
+++ Assets/FPS Controller/Scripts/PlayerMovement.cs
@@ -1,12 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController characterController;
     [SerializeField] private float movementSpeed;
+    
 
     [Header("Gravity Options")]
-    [SerializeField] [Range(-80f, 0f)] private float gravityScale = -40f;
+    [SerializeField] [Range(-10f, 10f)] private float gravityScale = -4f;
     [SerializeField] [Range(2f, 30f)] private float jumpHeight = 5f;
 
     [Header("Ground Options")]
@@ -19,37 +21,58 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector3 movementDirection;
     private Vector3 verticalVelocity;
+    private float gravityMultiplyer = 20f;
     private bool canDoubleJump;
     private bool isGrounded;
+    private float sprintSpeed;
+    private float normalSpeed;
 
     public bool CanDoubleJump { get => canDoubleJump; set => canDoubleJump = value; }
     public bool IsGrounded { get => isGrounded; set => isGrounded = value; }
 
     void Start()
     {
+        normalSpeed = movementSpeed;
+        sprintSpeed = movementSpeed * 2;
+        
         characterController = GetComponent<CharacterController>();        
     }
 
     void Update()
     {
         GroundCheck();
+        PlayerSprint();
         HorizontalMovement();
         VerticalMovement();
         Jump();
 
     }
 
+    private void PlayerSprint()
+    {
+        if (Input.GetButton("Sprint"))
+        {
+
+            movementSpeed = sprintSpeed;
+        }
+
+        if (Input.GetButtonUp("Sprint"))
+        {
+            movementSpeed = normalSpeed;
+        }
+    }
+
     private void Jump()
     {
         if (IsGrounded && Input.GetButtonDown(jumpButton))
         {
-            verticalVelocity.y = Mathf.Sqrt(-2 * jumpHeight * gravityScale);
+            verticalVelocity.y = Mathf.Sqrt(-2 * jumpHeight * gravityScale * gravityMultiplyer);
             CanDoubleJump = true;
         }
         else if (Input.GetButtonDown(jumpButton) && CanDoubleJump)
         {
             CanDoubleJump = false;
-            verticalVelocity.y = Mathf.Sqrt(-2 * jumpHeight * gravityScale);
+            verticalVelocity.y = Mathf.Sqrt(-2 * jumpHeight * gravityScale * gravityMultiplyer);
         }
     }
 
@@ -61,7 +84,7 @@ public class PlayerMovement : MonoBehaviour
         }
         else
         {
-            verticalVelocity.y += gravityScale * Time.deltaTime;
+            verticalVelocity.y += gravityScale * gravityMultiplyer * Time.deltaTime;
         }
         characterController.Move(verticalVelocity * Time.deltaTime);
     }
