45962154dff7aede011dbde99b1f6c6ead88eb30
Turkay Tunc <turkaytunc00@gmail.com>
2020-05-18 12:09:57 +0300

    update: enemy movement vector calculation

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -4,28 +4,28 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    [SerializeField] private Transform followTransform;
-
+    private Transform followTransform;
     private NavMeshAgent navMeshAgent;
-    private float followDistance;
-    private float maxChaseRadius = 20f;
-    private float minChaseRadius = 2f;
-    private float movementSpeed = 7f;
+
+    private float chaseDistance;
+
+    [Header("Enemy Movement Controls")]
+    [SerializeField] private float maxChaseRadius = 20f;
+    [SerializeField] private float minChaseRadius = 2f;
+    [SerializeField] private float movementSpeed = 7f;
 
 
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         followTransform = FindObjectOfType<PlayerMovement>().transform;
-        
     }
 
     void Update()
     {
-        followDistance = Vector3.Distance(followTransform.position, transform.position);
+        chaseDistance = Vector3.Distance(followTransform.position, transform.position);
 
-
-        if ( followDistance > minChaseRadius && followDistance < maxChaseRadius )
+        if ( chaseDistance > minChaseRadius && chaseDistance < maxChaseRadius )
         {
             Vector3 offset = (followTransform.position - transform.position).normalized * Time.deltaTime * movementSpeed;
 
@@ -35,13 +35,10 @@ public class EnemyAI : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-
-
         Handles.color = new Vector4(255, 0, 0, 0.2f);
         Handles.DrawSolidDisc(transform.position + Vector3.down, Vector3.up, maxChaseRadius);
 
         Handles.color = new Vector4(0, 255, 0, 0.2f);
         Handles.DrawSolidDisc(transform.position + Vector3.down * 0.5f, Vector3.up, minChaseRadius);
-
     }
 }
