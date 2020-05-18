1ee9c4d606ed443fde84826c41203ea6aad6a54f
Turkay Tunc <turkaytunc00@gmail.com>
2020-05-18 11:58:18 +0300

    added: enemy follow script

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,46 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private Transform followTransform;
+
+    private NavMeshAgent navMeshAgent;
+    private float followDistance;
+    private float maxChaseRadius = 20f;
+    private float minChaseRadius = 2f;
+    private float movementSpeed = .5f;
+
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        followTransform = FindObjectOfType<PlayerMovement>().transform;
+        
+    }
+
+    void Update()
+    {
+        followDistance = Vector3.Distance(followTransform.position, transform.position);
+
+
+        if ( followDistance > minChaseRadius && followDistance < maxChaseRadius )
+        {
+
+            navMeshAgent.Move((followTransform.position - transform.position) * Time.deltaTime * movementSpeed);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+
+
+        Handles.color = new Vector4(255, 0, 0, 0.2f);
+        Handles.DrawSolidDisc(transform.position + Vector3.down, Vector3.up, maxChaseRadius);
+
+        Handles.color = new Vector4(0, 255, 0, 0.2f);
+        Handles.DrawSolidDisc(transform.position + Vector3.down * 0.5f, Vector3.up, minChaseRadius);
+
+    }
+}
